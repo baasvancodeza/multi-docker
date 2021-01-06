a9b6f3cbe54b46697a3e2dc98f64d2c5c17129dc
Stefan <baasvancode@gmail.com>
2021-01-06 11:57:03 +0000

    changed text

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <Link to="/">Home</Link>
+          <Link to="/">Home FibCalc</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
